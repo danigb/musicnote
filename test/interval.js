7fdbadc65064539c99d2541d211fa71855d5e68b
danigb <danigb@gmail.com>
2015-05-03 07:02:26 +0200

    Split files: interval, note

--- test/interval.js
+++ test/interval.js
@@ -1,23 +1,44 @@
 var vows = require('vows');
 var assert = require('assert');
-var interval = require('../note.js').interval;
+var interval = require('../lib/interval.js');
 
+assert.interval = function(i, steps, distance) {
+  assert.deepEqual([i.steps, i.distance], [steps, distance]);
+}
 
-vows.describe('intervals').addBatch({
-  "quality": function() {
-    assert.equal(interval('M2').quality, 'M');
-    assert.equal(interval('-M2').quality, 'M');
-  },
-  "direction": function() {
-    assert.equal(interval('M2').dir, 1);
-    assert.equal(interval('-M2').dir, -1);
-  },
-  "octaves": function() {
-    assert.equal(interval("M3").oct, 0);
-    //assert.equal(interval("M11").oct, 1);
+vows.describe('Intervals').addBatch({
+  "constructor": {
+    "return an interval object": function() {
+      assert(interval(0, 0) instanceof interval);
+    },
+    "set steps and distance": function() {
+      assert.equal(interval(1, 4).steps, 1);
+      assert.equal(interval(1, 4).distance, 4);
+    },
+    "array": function() {
+      assert.equal(interval([1, 4]).steps, 1);
+      assert.equal(interval([1, 4]).distance, 4);
+    }
   },
   "simple": function() {
-    assert.equal(interval("M3").simple, 'M3');
-    //assert.equal(interval("-M9").simple, '-M2');
+    assert.interval(interval('P1'), 0, 0);
+    assert.interval(interval('A1'), 0, 1);
+    assert.interval(interval('d2'), 1, 0);
+    assert.interval(interval('m2'), 1, 1);
+    assert.interval(interval('M2'), 1, 2);
+    assert.interval(interval('A2'), 1, 3);
+  },
+  "negative": function() {
+    assert.interval(interval('P-1'), 0, 0);
+    assert.interval(interval('A-1'), 0, -1);
+  },
+  "octaves": function() {
+    assert.interval(interval('P8'), 7, 12);
+    assert.interval(interval('P-8'), -7, -12);
+  },
+  "toString": function() {
+    assert.equal(interval(1, 2).toString(), "M2");
+    assert.equal(interval(7, 12).toString(), "P8");
+    assert.equal(interval(-1, -2).toString(), "M-2");
   }
 }).export(module);
--- test/note.js
+++ test/note.js
@@ -1,6 +1,6 @@
 var vows = require('vows');
 var assert = require('assert');
-var note = require('../note.js');
+var note = require('../lib/note.js');
 
 var equalNote = function(note, expected) {
   assert.deepEqual([note.name, note.oct], expected);
@@ -8,22 +8,29 @@ var equalNote = function(note, expected) {
 
 vows.describe('note').addBatch({
   "constructor": {
+    "return same object if is a note": function() {
+      var c = note('c');
+      assert(c === note(c));
+    },
     "constructor returns an instance of note": function() {
       assert(note('c') instanceof note);
     },
     "empty constructors returns middle C": function() {
       equalNote(note(), ['c', 4]);
     },
+    "name, octave": function() {
+      equalNote(note("gb", 6), ["gb", 6]);
+    },
+    "default octave": function() {
+      equalNote(note("db"), ["db", 4]);
+    },
     "impro-visor notation": function() {
       equalNote(note('c#'), ['c#', 4]);
       equalNote(note('db+'), ['db', 5]);
       equalNote(note('b--'), ['b', 2]);
     },
-    "array": function() {
+    "array notation": function() {
       equalNote(note(['c#', 3]), ['c#', 3]);
-    },
-    "arguments": function() {
-      equalNote(note("gb", 6), ["gb", 6]);
     }
   },
   "midi": {
@@ -31,15 +38,42 @@ vows.describe('note').addBatch({
       assert.equal(note('c', 4).midi(), 60);
     }
   },
+  "note shift": function() {
+    equalNote(note('c').shift(0), ['c', 4]);
+    equalNote(note('b').shift(1), ['c', 4]);
+    equalNote(note('c').shift(7), ['c', 4]);
+
+    equalNote(note('c').shift(-1), ['b', 4]);
+    equalNote(note('c').shift(-7), ['c', 4]);
+    equalNote(note('c').shift(-8), ['b', 4]);
+  },
   "note transpose": {
     "octaves": function() {
       equalNote(note('c', 4).transpose("P8"), ["c", 5]);
-      equalNote(note('g', 4).transpose("-P8"), ["g", 5]);
+      equalNote(note('g', 4).transpose("P-8"), ["g", 3]);
     },
     "M2": function() {
       equalNote(note('e').transpose("M2"), ['f#', 4]);
       equalNote(note('b').transpose("M2"), ['c#', 4]);
+    },
+    "M-2": function() {
+      equalNote(note('d').transpose('M-2'), ['c', 4]);
     }
+  },
+  "distance": {
+    "base": function() {
+      var c = note('c', 4);
+      assert.equal(c.distance(note('d', 4)).toString(), 'M2');
+      assert.equal(c.distance(note('g', 4)).toString(), 'P5');
+      assert.equal(c.distance(note('c', 5)).toString(), 'P8')
+      assert.equal(c.distance(note('d', 5)).toString(), 'M9');
+
+      assert.equal(c.distance(note('c', 3)).toString(), 'P-8')
+      assert.equal(c.distance(note('b', 3)).toString(), 'm-2');
+      assert.equal(c.distance(note('g', 3)).toString(), 'P-4');
+      assert.equal(c.distance(note('d', 3)).toString(), 'M-9');
+    }
+
   },
   "toString": {
     "scientific": function() {
